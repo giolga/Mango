afb50efc7b1a70b5aa28a403bd0dc8ac8c0d8507
giolga <giolga.kumi@gmail.com>
2025-02-19 20:19:54 +0400

    Base service logic added

--- Mango.Web/obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
+++ Mango.Web/obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9b97435c42dbae57d6b3eca6ca61bfbe94789a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd47464a87d5b178ab1a22e7be6276f8c04bcea3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
