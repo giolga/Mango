fb5a56442be3ca36026ce308d50dc7218ff4e161
giolga <giolga.kumi@gmail.com>
2025-02-23 16:25:44 +0400

    Identity user service started:

--- Mango.Web/obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
+++ Mango.Web/obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+369d682de6334f0fa021b2bb423d40ea9d69a40b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bfb330dc5db49c16e1df9293506aff793cb2493")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
