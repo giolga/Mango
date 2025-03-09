a4387a89dae8373c44dcab970240a32b23bae677
giolga <giolga.kumi@gmail.com>
2025-03-09 14:20:42 +0400

    Product Api

---
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,126 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.Data;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [Route("api/product")]
+    [ApiController]
+    //[Authorize]
+    public class ProductAPIController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private ResponseDto _response;
+        private readonly IMapper _mapper;
+
+        public ProductAPIController(AppDbContext context, ResponseDto response, IMapper mapper)
+        {
+            this._context = context;
+            this._response = response;
+            this._mapper = mapper;
+        }
+
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                IEnumerable<Product> objList = _context.Products.ToList();
+                _response.Result = _mapper.Map<IEnumerable<ProductDto>>(objList);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto Get(int id)
+        {
+            try
+            {
+                Product obj = _context.Products.First(c => c.ProductId == id);
+                if (obj is null)
+                {
+                    _response.IsSuccess = false;
+                }
+                _response.Result = _mapper.Map<ProductDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+
+        [HttpPost]
+        //[Authorize(Roles = "Admin")]
+        public ResponseDto Post([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                Product product = _mapper.Map<Product>(productDto);
+                _context.Products.Add(product);
+                _context.SaveChanges();
+                _response.Result = _mapper.Map<ProductDto>(product);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpPut]
+        //[Authorize(Roles = "Admin")]
+        public ResponseDto Put([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                Product obj = _mapper.Map<Product>(productDto);
+                _context.Products.Update(obj);
+                _context.SaveChanges();
+
+                _response.Result = _mapper.Map<ProductDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        //[Authorize(Roles = "Admin")]
+        public ResponseDto Delete(int id)
+        {
+            try
+            {
+                Product obj = _context.Products.First(c => c.ProductId == id);
+                _context.Products.Remove(obj);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+    }
+}
